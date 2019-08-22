167a5f3b1c15f99949bcc83e71afb56f7898c33f
Nikola Vojvodic <nikola.vojvodic0@gmail.com>
2019-08-22 04:55:48 +0200

    Home page hotels tab table

--- src/main/webapp/app/components/homeTabs/hotels-tab/hotels-tab.component.ts
+++ src/main/webapp/app/components/homeTabs/hotels-tab/hotels-tab.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IHotel } from 'app/shared/model/hotel.model';
+import { HotelService } from 'app/entities/hotel/hotel.service';
+import { JhiAlertService } from 'ng-jhipster';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
     selector: 'jhi-hotels-tab',
@@ -6,7 +10,25 @@ import { Component, OnInit } from '@angular/core';
     styles: []
 })
 export class HotelsTabComponent implements OnInit {
-    constructor() {}
+    formData: Object;
+    hotels: IHotel[];
+
+    constructor(protected hotelService: HotelService, protected jhiAlertService: JhiAlertService) {
+        this.getHotels();
+    }
 
     ngOnInit() {}
+
+    getHotels() {
+        this.hotelService.query().subscribe(
+            (res: HttpResponse<IHotel[]>) => {
+                this.hotels = res.body;
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+    }
+
+    protected onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
+    }
 }
