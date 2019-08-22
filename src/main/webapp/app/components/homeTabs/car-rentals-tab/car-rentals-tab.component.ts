1d73aab064027ba4b872447e6d2bb9c780b7ef9e
Nikola Vojvodic <nikola.vojvodic0@gmail.com>
2019-08-22 05:18:41 +0200

    Home page car rentals tab table

--- src/main/webapp/app/components/homeTabs/car-rentals-tab/car-rentals-tab.component.ts
+++ src/main/webapp/app/components/homeTabs/car-rentals-tab/car-rentals-tab.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ICompanyLocation } from '../../../shared/model/company-location.model';
+import { CompanyLocationService } from '../../../entities/company-location';
+import { JhiAlertService } from 'ng-jhipster';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
     selector: 'jhi-car-rentals-tab',
@@ -6,6 +10,25 @@ import { Component, OnInit } from '@angular/core';
     styles: []
 })
 export class CarRentalsTabComponent implements OnInit {
-    constructor() {}
-    ngOnInit() {}
+    formData: Object;
+    companyLocations: ICompanyLocation[];
+
+    constructor(protected companyLocationService: CompanyLocationService, protected jhiAlertService: JhiAlertService) {}
+
+    ngOnInit() {
+        this.getCompanyLocations();
+    }
+
+    getCompanyLocations() {
+        this.companyLocationService.query().subscribe(
+            (res: HttpResponse<ICompanyLocation[]>) => {
+                this.companyLocations = res.body;
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+    }
+
+    protected onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
+    }
 }
--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -4,13 +4,20 @@ import { NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
 import { NgbDateMomentAdapter } from './util/datepicker-adapter';
 import { PlaneTicketsSharedLibsModule, PlaneTicketsSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective } from './';
 import { CompanyTypeFilterPipe } from './pipes/company-type-filter.pipe';
+import { CompanyLocationTypeFilterPipe } from './pipes/company-location-type-filter.pipe';
 
 @NgModule({
     imports: [PlaneTicketsSharedLibsModule, PlaneTicketsSharedCommonModule],
-    declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective, CompanyTypeFilterPipe],
+    declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective, CompanyTypeFilterPipe, CompanyLocationTypeFilterPipe],
     providers: [{ provide: NgbDateAdapter, useClass: NgbDateMomentAdapter }],
     entryComponents: [JhiLoginModalComponent],
-    exports: [PlaneTicketsSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective, CompanyTypeFilterPipe],
+    exports: [
+        PlaneTicketsSharedCommonModule,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        CompanyTypeFilterPipe,
+        CompanyLocationTypeFilterPipe
+    ],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class PlaneTicketsSharedModule {
